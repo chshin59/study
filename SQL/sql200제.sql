12fd16fabe0dac100e407d195d941899a9cfb5a0
chshin59 <chshin59@gmail.com>
2022-07-06 04:25:32 +0900

    SQL sql200제 12~15

--- SQL/sql200제.sql
+++ SQL/sql200제.sql
@@ -42,10 +42,139 @@ SELECT sal + NVL(comm, 0) FROM emp WHERE ename='KING';
 -- 010 비교 연산자
 SELECT ename, sal, job, deptno FROM emp WHERE sal <= 1200;
 
--- 011 비교 연산자
+-- 011 비교 연산자(BETWEEN AND)
 SELECT ename, sal FROM emp WHERE sal BETWEEN 1000 AND 3000;
 SELECT ename, sal FROM emp WHERE (sal >= 1000 AND sal <= 3000);
 SELECT ename, sal FROM emp WHERE sal NOT BETWEEN 1000 AND 3000;
-SELECT ename, hiredate
-    FROM emp
-    WHERE hiredate BETWEEN '1982/01/01' and '1982/12/31';
+SELECT ename, hiredate FROM emp WHERE hiredate BETWEEN '1982/01/01' and '1982/12/31';
+
+-- 012 비교 연산자(LIKE)
+SELECT ename, sal FROM emp WHERE ename LIKE 'S%';
+SELECT ename FROM emp WHERE ename LIKE '_M%';
+SELECT ename FROM emp WHERE ename LIKE '%T';
+SELECT ename FROM emp WHERE ename LIKE '%A%';
+
+-- 013 비교 연산자(IS NULL)
+SELECT ename, comm FROM emp WHERE comm is null; 
+
+-- 014 비교 연산자(IN)
+SELECT ename, sal, job FROM emp WHERE job in ('SALESMAN','ANALYST','MANAGER'); 
+SELECT ename, sal, job FROM emp WHERE (job ='SALESMAN' or job='ANALYST' or job='MANAGER'); 
+SELECT ename, sal, job FROM emp WHERE job NOT in ('SALESMAN','ANALYST','MANAGER'); 
+SELECT ename, sal, job FROM emp WHERE (job !='SALESMAN' and job !='ANALYST' and job !='MANAGER'); 
+
+-- 015 비교 연산자(AND, OR, NOT)
+SELECT ename, sal, job FROM emp WHERE job='SALESMAN' AND sal >= 1200; 
+SELECT ename, sal, job FROM emp WHERE job='ABCDEFG' AND sal >= 1200; 
+
+-----------------------------------------------------------
+
+-- 056
+
+-- 057
+
+-- 058
+
+-- 059
+
+-- 060
+
+-- 061
+
+-- 062
+
+-- 063
+
+-- 064
+
+-- 065
+
+-- 066
+
+-- 067
+
+-- 068
+
+-- 069
+
+-- 070
+
+-- 071
+
+-- 072
+
+-- 073
+
+-- 074
+
+-- 075
+
+-- 076
+
+-- 077
+
+-- 078
+
+-- 079
+
+-- 080
+
+-- 081
+
+-- 082
+
+-- 083
+
+-- 084
+
+-- 085
+
+-- 086
+
+-- 087
+
+-- 088
+
+-- 089
+
+-- 090
+
+-- 091
+
+-- 092
+
+-- 093
+
+-- 094
+
+-- 095
+
+-- 096
+
+-- 097
+
+-- 098
+
+-- 099
+
+-- 100
+
+-- 101
+
+-- 102
+
+-- 103
+
+-- 104
+
+-- 105
+
+-- 106
+
+-- 107
+
+-- 108
+
+-- 109
+
+-- 110
