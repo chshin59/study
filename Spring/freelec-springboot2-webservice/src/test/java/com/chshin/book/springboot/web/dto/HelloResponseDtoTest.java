3deaf2362ef1b94e1263fade5317cc80fed91eea
chshin59 <chshin59@gmail.com>
2023-03-07 07:13:39 +0900

    Spring 스프링부트 Ch2. 롬복 설치 및 테스트 코드 작성

---
+++ Spring/freelec-springboot2-webservice/src/test/java/com/chshin/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.chshin.book.springboot.web.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
