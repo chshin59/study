d5c4c4e5bdd627d9a41949df6b84a77758f5e900
chshin59 <chshin59@gmail.com>
2023-03-13 20:12:28 +0900

    Spring 스프링부트 Ch3. JPA를 이용한 REST API 생성

---
+++ Spring/freelec-springboot2-webservice/src/main/java/com/chshin/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.chshin.book.springboot.web.dto;
+
+import com.chshin.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
---
+++ Spring/freelec-springboot2-webservice/src/test/java/com/chshin/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.chshin.book.springboot.domain.posts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        postsRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author("chshin")
+            .build()
+        );
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void registerBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2023, 3, 13, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>>>> createdDate=" + posts.getCreatedDate() +
+            ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
